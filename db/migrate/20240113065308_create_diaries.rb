42216e15b899475f77a071436dcb512197d13704
EC2 Default User <ec2-user@ip-172-31-46-71.ap-northeast-1.compute.internal>
2024-01-27 07:12:06 +0000

    Diaryのマイグレーションファイルを変更

--- db/migrate/20240113065308_create_diaries.rb
+++ db/migrate/20240113065308_create_diaries.rb
@@ -1,8 +1,8 @@
 class CreateDiaries < ActiveRecord::Migration[6.1]
   def change
     create_table :diaries do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :team, index: true, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :team, foreign_key: true
       t.string :title
       t.text :body
 
--- db/migrate/20240127064456_change_user_column_type_in_diaries_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserColumnTypeInDiariesTable < ActiveRecord::Migration[6.1]
-  def change
-    change_column :diaries, :user_id, :bigint
-  end
-end
